1330cbbdbb3bb4e75a91f817cff2eee6bb762d0e
Тишкин Андрей Алексеевич <74547328+andrei1112111@users.noreply.github.com>
2025-02-04 14:14:27 +0700

    add ping time

--- 1/main.py
+++ 1/main.py
@@ -1,4 +1,7 @@
 import os
+import subprocess
+import re
+
 import pandas as pd
 
 
@@ -8,8 +11,12 @@ df = pd.read_csv("hostnames.csv")
 for i in df.index:
     if os.system(f"ping -c 1 {df.at[i, 'hostname']}") != 0:
         df.at[i, "ping_status"] = 'err'
+        df.at[i, "time"] = '-1'
     else:
         df.at[i, "ping_status"] = 'ok'
+
+        output = subprocess.check_output(f"ping -c 1 {df.at[i, 'hostname']}", shell=True)
+        df.at[i, "time"] = re.findall(r"time\S+", str(output))[0][5:]
 
 
 df.to_csv("hostnames.csv", index=False)
