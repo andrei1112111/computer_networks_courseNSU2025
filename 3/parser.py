69ad14d20bd45c190477f8efec7df4487b580916
Тишкин Андрей Алексеевич <74547328+andrei1112111@users.noreply.github.com>
2025-02-18 13:25:22 +0700

    lab3

--- 3/parser.py
+++ 3/parser.py
@@ -1,15 +1,14 @@
 from selenium import webdriver
 
+
 driver_path = '/path/to/chromedriver'
 driver = webdriver.Chrome(executable_path=driver_path)
 driver.get('https://www.example.com/form')
 
-# Найдите элементы формы
 username = driver.find_element_by_name('username')
 password = driver.find_element_by_name('password')
 submit_button = driver.find_element_by_xpath('//button[@type="submit"]')
 
-# Взаимодействуйте с элементами
 username.send_keys('my_username')
 password.send_keys('my_password')
 submit_button.click()
